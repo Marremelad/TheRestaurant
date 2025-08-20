54252ee7e443e9d1cfaf3df62bcf27e8f3e0d9f6
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-20 09:14:05 +0200

    Configured db settings and added connection string to settings file

--- TheRestaurant/Program.cs
+++ TheRestaurant/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TheRestaurant.Data;
+
 namespace TheRestaurant;
 
 public class Program
@@ -7,6 +10,11 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+
+        builder.Services.AddDbContext<TheRestaurantDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
