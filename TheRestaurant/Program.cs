b95bb205eed1dd88e4e2f9f8b29c7a7110cc06d2
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-21 08:22:55 +0200

    Registered TableRepository to DI container

--- TheRestaurant/Program.cs
+++ TheRestaurant/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TheRestaurant.Data;
+using TheRestaurant.Repositories;
+using TheRestaurant.Repositories.IRepositories;
 
 namespace TheRestaurant;
 
@@ -10,11 +12,12 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddDbContext<TheRestaurantDbContext>(options =>
         {
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
+
+        builder.Services.AddScoped<ITableRepository, TableRepository>();
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
