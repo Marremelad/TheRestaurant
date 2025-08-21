baae6a4ba39e605c8c355c08cd7b2d3ed0aa4389
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-21 08:21:12 +0200

    Implemented TableRepository methods

---
+++ TheRestaurant/Repositories/TableRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheRestaurant.Data;
+using TheRestaurant.Models;
+using TheRestaurant.Repositories.IRepositories;
+
+namespace TheRestaurant.Repositories;
+
+public class TableRepository(TheRestaurantDbContext context) : ITableRepository
+{
+    public async Task<IEnumerable<Table>> GetTablesAsync() => 
+        await context.Tables.ToListAsync();
+
+    public async Task<Table> GetTableAsync(int tableId) =>
+        (await context.Tables.SingleOrDefaultAsync(table => table.Id == tableId))!;
+
+    public async Task<int> CreateTableAsync(Table newTable)
+    {
+        context.Tables.Add(newTable);
+        await context.SaveChangesAsync();
+        return newTable.Id;
+    }
+}
