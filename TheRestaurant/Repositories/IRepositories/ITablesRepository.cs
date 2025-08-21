989e25e70c3158df0018b5d582112a28bc4c0c2f
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-21 07:58:27 +0200

    Created method signatures in ITablesRepository interface

--- TheRestaurant/Repositories/IRepositories/ITablesRepository.cs
+++ TheRestaurant/Repositories/IRepositories/ITablesRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace TheRestaurant.Repositories.IRepositories;
+﻿using TheRestaurant.Models;
+
+namespace TheRestaurant.Repositories.IRepositories;
 
 public interface ITablesRepository
-{
-    
+{ 
+    Task<IEnumerable<Table>> GetTablesAsync();
+
+    Task<Table> GetTableAsync(int tableId);
+
+    Task<int> CreateTableAsync(Table newTable);
 }
+
