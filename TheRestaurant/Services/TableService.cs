7cba7d133e4ecdc27ec5bf8844399460fa6591b6
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-23 15:34:30 +0200

    Implemented GetAllTables method for TableService

--- TheRestaurant/Services/TableService.cs
+++ TheRestaurant/Services/TableService.cs
@@ -1,6 +1,23 @@
-﻿namespace TheRestaurant.Services;
+﻿using TheRestaurant.DTOs;
+using TheRestaurant.Mappers;
+using TheRestaurant.Models;
+using TheRestaurant.Repositories.IRepositories;
+using TheRestaurant.Services.IServices;
 
-public class TableService
+namespace TheRestaurant.Services;
+
+public class TableService(ITableRepository repository) : ITableService
 {
-    
+    public async Task<IEnumerable<TableDto>> GetTablesAsync() =>
+        TableMapper.ToDtos(await repository.GetTablesAsync());
+
+    public Task<Table?> GetTableAsync(int tableId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> CreateTableAsync(Table newTable)
+    {
+        throw new NotImplementedException();
+    }
 }
