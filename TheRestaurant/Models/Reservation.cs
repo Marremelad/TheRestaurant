267232312e03f870be0bf089450df03d09d53d82
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-23 18:24:15 +0200

    Created Reservation model

---
+++ TheRestaurant/Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheRestaurant.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    
+    public DateOnly Date { get; set; }
+    
+    public int TimeSlot { get; set; }
+    
+    [ForeignKey("Table")]
+    public required int TableId { get; set; }
+    public virtual Table? Table { get; set; }
+    
+    public required string FirstName { get; set; }
+    
+    public required string LastName { get; set; }
+    
+    public required string Email { get; set; }
+}
