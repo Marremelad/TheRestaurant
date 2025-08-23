5bc40c62465875483bc60b4517805382830f0782
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-23 15:34:03 +0200

    Defined method signatures for ITableService

--- TheRestaurant/Services/IServices/ITableService.cs
+++ TheRestaurant/Services/IServices/ITableService.cs
@@ -1,6 +1,13 @@
-﻿namespace TheRestaurant.Services.IServices;
+﻿using TheRestaurant.DTOs;
+using TheRestaurant.Models;
+
+namespace TheRestaurant.Services.IServices;
 
 public interface ITableService
 {
-    
+    Task<IEnumerable<TableDto>> GetTablesAsync();
+
+    Task<Table?> GetTableAsync(int tableId);
+
+    Task<int> CreateTableAsync(Table newTable);
 }
