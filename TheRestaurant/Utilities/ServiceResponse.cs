22e8d2c92ad5221791cc8fec5a1fb7e09e011617
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-23 16:14:07 +0200

    Created ServiceResponse class

---
+++ TheRestaurant/Utilities/ServiceResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace TheRestaurant.Utilities;
+
+public record ServiceResponse<T>(HttpStatusCode StatusCode, T? Value, string Message)
+{
+    public static ServiceResponse<T> Success(HttpStatusCode statusCode, T value, string message) =>
+        new(statusCode, value, message);
+
+    public static ServiceResponse<T> Failure(HttpStatusCode statusCode, string message) =>
+        new(statusCode, default, message);
+}
