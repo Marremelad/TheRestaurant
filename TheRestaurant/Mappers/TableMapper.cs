d39f23ea33d8a103b27a7058b7e5b8ac58a6e919
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-23 15:32:37 +0200

    Created mapping class for Table model

--- TheRestaurant/Mappers/TableMapper.cs
+++ TheRestaurant/Mappers/TableMapper.cs
@@ -1,6 +1,13 @@
-﻿namespace TheRestaurant.Mappers;
+﻿using TheRestaurant.DTOs;
+using TheRestaurant.Models;
+
+namespace TheRestaurant.Mappers;
 
 public class TableMapper
 {
+    public static TableDto ToDto(Table table) => new(table.Number, table.Capacity);
+    public static IEnumerable<TableDto> ToDtos(IEnumerable<Table> tables) => tables.Select(ToDto);
     
+    public static Table ToEntity(TableDto tableDto) => new() { Number = tableDto.Number, Capacity = tableDto.Capacity };
+    public static IEnumerable<Table> ToEntities(IEnumerable<TableDto> tableDtos) => tableDtos.Select(ToEntity);
 }
