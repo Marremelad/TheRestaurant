a97e7dd35720ae65b457538d5061bc5aaccd0cb9
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-23 15:31:48 +0200

    Implemented get endpoint for all tables

--- TheRestaurant/Controllers/TablesController.cs
+++ TheRestaurant/Controllers/TablesController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TheRestaurant.DTOs;
+using TheRestaurant.Services.IServices;
 
 namespace TheRestaurant.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class TablesController : ControllerBase
+public class TablesController(ITableService service) : ControllerBase
 {
-    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TableDto>>> GetTables()
+    {
+        return Ok(await service.GetTablesAsync());
+    }
 }
