24b1de701b2d8abc724801ad7ff9e12354c3ba16
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-23 17:04:50 +0200

    Added error handling to GetAllTables method

--- TheRestaurant/Services/TableService.cs
+++ TheRestaurant/Services/TableService.cs
@@ -1,15 +1,36 @@
-﻿using TheRestaurant.DTOs;
+﻿using System.Net;
+using TheRestaurant.DTOs;
 using TheRestaurant.Mappers;
 using TheRestaurant.Models;
 using TheRestaurant.Repositories.IRepositories;
 using TheRestaurant.Services.IServices;
+using TheRestaurant.Utilities;
 
 namespace TheRestaurant.Services;
 
-public class TableService(ITableRepository repository) : ITableService
+public class TableService(
+    ITableRepository repository,
+    ILogger<TableService> logger
+    ) : ITableService
 {
-    public async Task<IEnumerable<TableDto>> GetTablesAsync() =>
-        TableMapper.ToDtos(await repository.GetTablesAsync());
+    public async Task<ServiceResponse<IEnumerable<TableDto>>> GetTablesAsync()
+    {
+        try
+        {
+            return ServiceResponse<IEnumerable<TableDto>>.Success(
+                HttpStatusCode.OK,
+                TableMapper.ToDtos(await repository.GetTablesAsync()),
+                "Tables fetched successfully."
+            );
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An Error occured while fetching tables.");
+            return ServiceResponse<IEnumerable<TableDto>>.Failure(
+                HttpStatusCode.InternalServerError,
+                "An error occurred while fetching tables.");
+        }
+    }
 
     public Task<Table?> GetTableAsync(int tableId)
     {
