42d4fa49c6c8eca55bfce10e46913f578686ecf8
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-24 09:24:50 +0200

    Added length restriction annotations to properties in Reservation model

--- TheRestaurant/Models/Reservation.cs
+++ TheRestaurant/Models/Reservation.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheRestaurant.Models;
 
@@ -14,9 +15,12 @@ public class Reservation
     public required int TableId { get; set; }
     public virtual Table? Table { get; set; }
     
+    [StringLength(50)]
     public required string FirstName { get; set; }
     
+    [StringLength(50)]
     public required string LastName { get; set; }
     
+    [StringLength(254)]
     public required string Email { get; set; }
 }
