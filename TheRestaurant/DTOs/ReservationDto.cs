45bb01da2e74e06c6b63c7fd2e6ec14ab879e6c8
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-24 10:03:15 +0200

    Created ReservationDto

--- TheRestaurant/DTOs/ReservationDto.cs
+++ TheRestaurant/DTOs/ReservationDto.cs
@@ -1,3 +1,10 @@
 ﻿namespace TheRestaurant.DTOs;
 
-public record ReservationDto();
+public record ReservationDto(
+    DateOnly Date,
+    int TimeSlot,
+    int TableId,
+    string FirstName,
+    string LastName,
+    string Email
+    );
