81e1928eade11b4869b826b7e890a72bfa4ceeaa
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-24 09:13:59 +0200

    Explicitly declared enum values

--- TheRestaurant/Enums/TimeSlot.cs
+++ TheRestaurant/Enums/TimeSlot.cs
@@ -3,9 +3,9 @@
 public enum TimeSlot
 {
     /// <summary>10:00 - 12:00</summary>
-    Slot10To12,
+    Slot10To12 = 1,
     /// <summary>12:00 - 14:00</summary>
-    Slot12To14,
+    Slot12To14 = 2,
     /// <summary>14:00 - 16:00</summary>
-    Slot14To16
+    Slot14To16 = 3
 }
