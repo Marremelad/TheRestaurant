d49ae34e5e014d0940f3931cc70c099a7d7b8302
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-24 09:23:26 +0200

    Created ReservationRepository class file

---
+++ TheRestaurant/Repositories/ReservationRepository.cs
@@ -0,0 +1,14 @@
+﻿using TheRestaurant.Data;
+using TheRestaurant.Models;
+using TheRestaurant.Repositories.IRepositories;
+using TheRestaurant.Utilities;
+
+namespace TheRestaurant.Repositories;
+
+public class ReservationRepository(TheRestaurantDbContext context): IReservationRepository
+{
+    public Task<Unit> CreateReservation(Reservation reservation)
+    {
+        throw new NotImplementedException();
+    }
+}
