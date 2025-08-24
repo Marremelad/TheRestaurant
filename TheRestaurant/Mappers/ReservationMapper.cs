1edfa448edb0e8f37e726bb29a8a24d7794a1f55
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-24 10:34:16 +0200

    Implemented mapper for Reservation model

---
+++ TheRestaurant/Mappers/ReservationMapper.cs
@@ -0,0 +1,32 @@
+﻿using TheRestaurant.DTOs;
+using TheRestaurant.Models;
+
+namespace TheRestaurant.Mappers;
+
+public static class ReservationMapper
+{
+    public static ReservationDto ToDto(Reservation reservation) =>
+        new(reservation.Date,
+            reservation.TimeSlot,
+            reservation.TableNumber,
+            reservation.FirstName,
+            reservation.LastName,
+            reservation.Email);
+    
+    public static IEnumerable<ReservationDto> ToDtos(IEnumerable<Reservation> reservations) =>
+        reservations.Select(ToDto);
+
+    public static Reservation ToEntity(ReservationDto reservationDto) =>
+        new()
+        {
+            Date = reservationDto.Date,
+            TimeSlot = reservationDto.TimeSlot,
+            TableNumber = reservationDto.TableNumber,
+            FirstName = reservationDto.FirstName,
+            LastName = reservationDto.LastName,
+            Email = reservationDto.Email
+        };
+
+    public static IEnumerable<Reservation> ToEntities(IEnumerable<ReservationDto> reservationDtos) =>
+        reservationDtos.Select(ToEntity);
+}
