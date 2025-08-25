b0fa13e7dc621327b7221545b1ab156995c53956
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 16:19:14 +0200

    Implemented methods in ReservationRepository

--- TheRestaurant/Repositories/ReservationRepository.cs
+++ TheRestaurant/Repositories/ReservationRepository.cs
@@ -1,4 +1,5 @@
-﻿using TheRestaurant.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TheRestaurant.Data;
 using TheRestaurant.Models;
 using TheRestaurant.Repositories.IRepositories;
 using TheRestaurant.Utilities;
@@ -7,9 +8,22 @@ namespace TheRestaurant.Repositories;
 
 public class ReservationRepository(TheRestaurantDbContext context): IReservationRepository
 {
+    public async Task<IEnumerable<Reservation>> GetReservationsAsync() =>
+        await context.Reservations.ToListAsync();
+
+    public async Task<Reservation?> GetReservationAsync(int reservationId) =>
+        await context.Reservations.FirstOrDefaultAsync(reservation => reservation.Id == reservationId);
+    
     public async Task<Unit> CreateReservationAsync(Reservation reservation)
     {
         context.Reservations.Add(reservation);
+        await context.SaveChangesAsync();
+        return Unit.Value;
+    }
+
+    public async Task<Unit> DeleteReservationAsync(Reservation reservation)
+    {
+        context.Reservations.Remove(reservation);
         await context.SaveChangesAsync();
         return Unit.Value;
     }
