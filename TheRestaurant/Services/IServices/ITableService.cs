99f078befde582259090aee8b81fb656d1145c21
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 15:26:40 +0200

    Added 'DeleteTableAsync' method to ITableService

--- TheRestaurant/Services/IServices/ITableService.cs
+++ TheRestaurant/Services/IServices/ITableService.cs
@@ -7,9 +7,11 @@ public interface ITableService
 {
     Task<ServiceResponse<IEnumerable<TableDto>>> GetTablesAsync();
 
-    Task<ServiceResponse<TableDto?>> GetTableAsync(int tableId);
+    Task<ServiceResponse<TableDto?>> GetTableAsync(int tableNumber);
 
     Task<ServiceResponse<Unit>> CreateTableAsync(TableDto tableDto);
+
+    Task<ServiceResponse<Unit>> DeleteTableAsync(int tableNumber);
 
     Task<bool> IsUniqueEntity(int primaryKey);
 }
