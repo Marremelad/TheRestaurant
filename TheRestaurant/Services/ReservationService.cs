0993152e4a4e4c95eca080bc303e18594995c15f
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 14:30:47 +0200

    Implemented 'CreateReservationAsync' method in ReservationService

---
+++ TheRestaurant/Services/ReservationService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using TheRestaurant.DTOs;
+using TheRestaurant.Mappers;
+using TheRestaurant.Repositories.IRepositories;
+using TheRestaurant.Services.IServices;
+using TheRestaurant.Utilities;
+
+namespace TheRestaurant.Services;
+
+public class ReservationService(
+    IReservationRepository repository,
+    ILogger logger
+    ) : IReservationService
+{
+    public async Task<ServiceResponse<Unit>> CreateReservation(ReservationDto reservationDto)
+    {
+        try
+        {
+            var reservation = ReservationMapper.ToEntity(reservationDto);
+
+            return ServiceResponse<Unit>.Success(
+                HttpStatusCode.Created,
+                await repository.CreateReservation(reservation),
+                "The reservation was created successfully."
+                );
+        }
+        catch (Exception ex)
+        {
+            const string errorMessage = "An error occurred while trying to create a reservation.";
+            logger.LogError(errorMessage);
+            return ServiceResponse<Unit>.Failure(
+                HttpStatusCode.InternalServerError,
+                errorMessage);
+        }
+        
+    }
+}
