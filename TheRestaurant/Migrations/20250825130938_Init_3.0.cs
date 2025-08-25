05dcf409cb44292120fa41bae2934ec06ed462ba
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 15:09:57 +0200

    ADded migration 'Init_3.0'

---
+++ TheRestaurant/Migrations/20250825130938_Init_3.0.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheRestaurant.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init_30 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_Date_TimeSlot_TableNumber",
+                table: "Reservations",
+                columns: new[] { "Date", "TimeSlot", "TableNumber" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_Date_TimeSlot_TableNumber",
+                table: "Reservations");
+        }
+    }
+}
