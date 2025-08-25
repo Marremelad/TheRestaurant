b69b7b4ca66f6fbb9775c5cfb841464772bb5417
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 18:01:09 +0200

    Created endpoints for ReservationsController

--- TheRestaurant/Controllers/ReservationsController.cs
+++ TheRestaurant/Controllers/ReservationsController.cs
@@ -9,7 +9,19 @@ namespace TheRestaurant.Controllers;
 [ApiController]
 public class ReservationsController(IReservationService service)
 {
+    [HttpGet]
+    public async Task<IActionResult> GetReservations() =>
+        Generate.ActionResult(await service.GetReservationsAsync());
+
+    [HttpGet("{reservation-email}")]
+    public async Task<IActionResult> GetReservation([FromRoute(Name = "reservation-email")] string reservationEmail) =>
+        Generate.ActionResult(await service.GetReservationAsync(reservationEmail));
+    
     [HttpPost]
     public async Task<IActionResult> CreateReservation(ReservationDto reservationDto) =>
         Generate.ActionResult(await service.CreateReservationAsync(reservationDto));
+
+    [HttpDelete("{reservation-email}")]
+    public async Task<IActionResult> DeleteReservation([FromRoute(Name = "reservation-email")] string reservationEmail) => 
+        Generate.ActionResult(await service.DeleteReservationAsync(reservationEmail));
 }
