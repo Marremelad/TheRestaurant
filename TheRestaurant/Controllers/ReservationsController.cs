af7333e87635e187d95401727ecfce190ff650a0
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 14:55:05 +0200

    Implemented post method for ReservationsController

---
+++ TheRestaurant/Controllers/ReservationsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheRestaurant.DTOs;
+using TheRestaurant.Services.IServices;
+using TheRestaurant.Utilities;
+
+namespace TheRestaurant.Controllers;
+
+[Route("api/reservations")]
+[ApiController]
+public class ReservationsController(IReservationService service)
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateReservation(ReservationDto reservationDto) =>
+        Generate.ActionResult(await service.CreateReservationAsync(reservationDto));
+}
