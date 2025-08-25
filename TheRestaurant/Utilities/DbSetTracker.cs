b9ab7225f66b6588e636bd72587253a18e2cf100
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 18:22:30 +0200

    Created DbSetTracker class

---
+++ TheRestaurant/Utilities/DbSetTracker.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheRestaurant.Data;
+
+namespace TheRestaurant.Utilities;
+
+public class DbSetTracker(TheRestaurantDbContext context)
+{
+    public async Task<int> GetTablesCount() =>
+        await context.Tables.CountAsync();
+
+    public async Task<bool> TableExists(int primaryKey) =>
+        await context.Tables.AnyAsync(table => table.Number == primaryKey);
+}
