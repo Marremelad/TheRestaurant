87967d20e540c7212ec32622ec7f6cb7512b533b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 08:22:31 +0200

    Added enpoints for get by tableNumber and post

--- TheRestaurant/Controllers/TablesController.cs
+++ TheRestaurant/Controllers/TablesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TheRestaurant.DTOs;
 using TheRestaurant.Services.IServices;
 using TheRestaurant.Utilities;
 
@@ -10,4 +11,12 @@ public class TablesController(ITableService service) : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetTables() =>
         Generate.ActionResult(await service.GetTablesAsync());
+
+    [HttpGet("{tableNumber:int}")]
+    public async Task<IActionResult> GetTable(int tableNumber) => 
+        Generate.ActionResult(await service.GetTableAsync(tableNumber));
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTable(TableDto tableDto) =>
+        Generate.ActionResult(await service.CreateTableAsync(tableDto));
 }
