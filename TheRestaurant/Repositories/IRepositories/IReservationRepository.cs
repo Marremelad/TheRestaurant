65f8017030e786585c597c2fd6c2c1802bf04756
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-25 16:10:53 +0200

    Added method signatures to 'IReservationRepository'

--- TheRestaurant/Repositories/IRepositories/IReservationRepository.cs
+++ TheRestaurant/Repositories/IRepositories/IReservationRepository.cs
@@ -5,5 +5,11 @@ namespace TheRestaurant.Repositories.IRepositories;
 
 public interface IReservationRepository
 {
+    Task<IEnumerable<Reservation>> GetReservationsAsync();
+
+    Task<Reservation> GetReservationAsync(int reservationId);
+    
     Task<Unit> CreateReservationAsync(Reservation reservation);
+
+    Task<Unit> DeleteReservationAsync(string reservationEmail);
 }
