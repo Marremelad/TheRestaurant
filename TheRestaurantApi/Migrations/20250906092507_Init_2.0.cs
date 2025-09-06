3b3ab6e2340e3e522cb66561f597c1ef0d4ac9da
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-09-06 11:25:29 +0200

    Added migration 'Init_2.0'

---
+++ TheRestaurantApi/Migrations/20250906092507_Init_2.0.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheRestaurant.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init_20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPopular",
+                table: "MenuItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsPopular",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsPopular",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsPopular",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsPopular",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "IsPopular",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2025, 9, 6, 9, 25, 7, 154, DateTimeKind.Utc).AddTicks(8402), "$2a$11$39dCki4lvlBDLWh.3lBoqOwNqLR4kAO9BCzz2uZQX/LyNaHztQoFW" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPopular",
+                table: "MenuItems");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2025, 9, 6, 8, 15, 41, 711, DateTimeKind.Utc).AddTicks(5993), "$2a$11$jR48bduPIbHSJeGx09Cmh.avy7D5GMnQCZ/uCcFonScoLh5Su7SrK" });
+        }
+    }
+}
